9962e22511d6075f96830422e1ec395a2bab9619
DJReyes10 <djreyesp@unah.hn>
2025-06-30 14:31:45 -0600

    feat: agregar pipes de fecha y numéricos en componentes básicos y de números

--- src/app/pages/basic-page/basic-page.component.ts
+++ src/app/pages/basic-page/basic-page.component.ts
@@ -1,13 +1,46 @@
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, ChangeDetectionStrategy, signal } from '@angular/core';
+import {
+  DatePipe,
+  LowerCasePipe,
+  TitleCasePipe,
+  UpperCasePipe,
+} from '@angular/common';
+import {
+  Component,
+  ChangeDetectionStrategy,
+  signal,
+  effect,
+} from '@angular/core';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
   nameLower = signal('douglas');
   nameUpper = signal('DOUGLAS');
   fullName = signal('DoUGlaS');
+
+  customDate = signal(new Date());
+
+  tickinDateEffect = effect(() => {
+    const interval = setInterval(() => {
+      this.customDate.set(new Date());
+      console.log('tick');
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  //TODO: Este es otro metodo que se puede usar para limpiar el efecto
+
+  //   tickinDateEffect = effect((onCleanup) => {
+  //     const interval = setInterval(() => {
+  //       this.customDate.set(new Date());
+  //       console.log('tick');
+  //     }, 1000);
+
+  //     onCleanup(() => clearInterval(interval));
+  //   });
+  // }
 }
--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe, CurrencyPipe, PercentPipe],
   templateUrl: './numbers-page.component.html',
 })
-export default class NumbersPageComponent {}
+export default class NumbersPageComponent {
+  totalSells = signal(1_234_567.8956);
+  percentage = signal(0.4856);
+}
